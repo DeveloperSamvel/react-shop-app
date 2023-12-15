99adae7b4cea5e0f70091bc288051cccf1b885bb
Samvel Vardanyan <129245937+DeveloperSamvel@users.noreply.github.com>
2023-12-15 19:33:30 +0400

    add loading for products component

---
+++ src/components/ProductList/Loading/Loading.component.js
@@ -0,0 +1,26 @@
+import './Loading.css';
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner">
+        <div className="loading-elements">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
--- src/components/ProductList/ProductList.component.js
+++ src/components/ProductList/ProductList.component.js
@@ -1,9 +1,11 @@
 import Card from './Card/Card.component';
+import Loading from './Loading/Loading.component';
 import './ProductList.css';
 
 const ProductList = ({products, onAddItem}) => {
   return (
     <div className='container'>
+      {products.length === 0 && <Loading />}
       {products.map((product) => (
         <Card
           img={product.photo}
